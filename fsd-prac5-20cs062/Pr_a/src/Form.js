b87149ed4936e3fe03a4c7abbcbb542f995bb8ea
iZERWAS <patilshivaraj65@gmail.com>
2022-08-25 12:35:18 +0530

    adding

---
+++ fsd-prac5-20cs062/Pr_a/src/Form.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './Form.css';
+import FormSignup from './FormSignup';
+import FormSuccess from './FormSuccess';
+
+const Form = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  function submitForm() {
+    setIsSubmitted(true);
+    alert("Form submitted!");
+  }
+  return (
+    <>
+      <div className='form-container'>
+        <span className='close-btn'>×</span>
+        <div className='form-content-left'>
+          <img className='form-img' src='https://pbs.twimg.com/card_img/1561428478674681856/Dfm3bTku?format=png&name=small' alt='spaceship' />
+        </div>
+        {!isSubmitted ? (
+          <FormSignup submitForm={submitForm} />
+        ) : (
+          <FormSuccess />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Form;
---
+++ fsd-prac5-20cs062/Pr_a/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <center><h1>Login Page</h1></center>
+    <App />
+    <footer style={{ textAlign: 'center' }}>20cs062 Shivaraj Patil</footer>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
